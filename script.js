6d8c04dcfe7ddd5086b01ba50614a56f76f34fb2
Ankit Gupta <ankitgupta@Ankits-MacBook-Air.local>
2026-01-25 23:50:53 +0530

    updated Portfolio

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior:'smooth' });
   });
 });
+
+const listItems = document.querySelectorAll('.cert-list li');
+  const images = document.querySelectorAll('.cert-img');
+
+  listItems.forEach(item => {
+    item.addEventListener('click', () => {
+      // Remove active from all
+      listItems.forEach(li => li.classList.remove('active'));
+      images.forEach(img => img.classList.remove('active'));
+
+      // Add active to clicked
+      item.classList.add('active');
+      const certId = item.getAttribute('data-cert');
+      document.getElementById(certId).classList.add('active');
+    });
+  });
